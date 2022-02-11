ba08a8acf74c26318c73088fee176af9a9ff7904
Adi Dzombic <dzombaxd@gmail.com>
2022-02-11 18:29:01 +0100

    Project created, Header section done
    Summary:
    + Project created
    + Firebase project created
    + Made Header Section

---
+++ src/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AppsIcon from "@mui/icons-material/Apps";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* this makes button clickable and with nice effect */}
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt="Gmail"
+        />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
